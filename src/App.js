cbf6e01508220127743e2a7f29b4bc3002e810c3
Trung Hoàng <trunghq3101@gmail.com>
2018-12-11 11:48:27 +0700

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import NavBar from './components/navBar/NavBar';
+import { Route, Switch } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Projects from './components/pages/Projects';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <NavBar/>
-                <div>MainContent</div>
+                <NavBar />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/home" component={Home} />
+                    <Route exact path="/projects" component={Projects} />
+                    <Route exact path="/contact" component={Contact} />
+                </Switch>
                 <div>Footer</div>
             </div>
         );
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styles from './NavBar.module.css'
+import { Link } from 'react-router-dom'
 
 export default (props) => {
     return (
-        <nav>
-            Home, Projects, Contact
+        <nav className={styles.NavBar}>
+            {["Home", "Projects", "Contact"].map(item => (
+                <div key={item} className={styles.NavItem}>
+                    <Link to={`/${item}`}>{item}</Link>
+                </div>
+            ))}
         </nav>
     )
 }
